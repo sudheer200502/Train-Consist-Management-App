f21ec9ebebfab17709ad14861f3bd39fbb70bbca
sudheer200502 <sudheerkoduru8500@gmail.com>
2026-04-22 11:21:02 +0530

    [sudheer] added [UC19]

--- App/src/TrainConsistManagementApp.java
+++ App/src/TrainConsistManagementApp.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
@@ -7,12 +9,25 @@ public class TrainConsistManagementApp {
         String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
         String searchKey = "BG309";
 
+        Arrays.sort(bogieIds);
+
+        int low = 0;
+        int high = bogieIds.length - 1;
         boolean found = false;
 
-        for (String id : bogieIds) {
-            if (id.equals(searchKey)) {
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            int cmp = searchKey.compareTo(bogieIds[mid]);
+
+            if (cmp == 0) {
                 found = true;
                 break;
+            } else if (cmp > 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
 
