d2c92f85f8b962baad4df0d04bae932cf116257e
sudheer200502 <sudheerkoduru8500@gmail.com>
2026-04-22 10:07:31 +0530

    [sudheer] added [UC2]

--- App/src/TrainConsistManagementApp.java
+++ App/src/TrainConsistManagementApp.java
@@ -7,9 +7,20 @@ public class TrainConsistManagementApp {
 
         System.out.println("=== Train Consist Management App ===");
 
-        List<String> trainConsist = new ArrayList<>();
+        List<String> passengerBogies = new ArrayList<>();
 
-        System.out.println("Train consist initialized.");
-        System.out.println("Initial number of bogies: " + trainConsist.size());
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+
+        System.out.println("Passenger bogies after addition: " + passengerBogies);
+
+        passengerBogies.remove("AC Chair");
+
+        System.out.println("Passenger bogies after removal: " + passengerBogies);
+
+        System.out.println("Does Sleeper exist? " + passengerBogies.contains("Sleeper"));
+
+        System.out.println("Final bogie list: " + passengerBogies);
     }
 }
